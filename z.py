fe90fd7e8f52484e4d5ba85b91f8310f581b4a65
gugugagagagagaga <146242047+gugugagagagagaga@users.noreply.github.com>
2023-10-25 19:54:19 +0200

    Add files via upload

---
+++ z.py
@@ -0,0 +1,219 @@
+import math
+
+#def Ppkwardatu(r):
+ #   return r**2
+#while True:
+  #   inp = input()
+   #  if inp == "a":
+  #        data = float(input())
+  #        print(Ppkwadratu(data))
+  #   elif inp == "b":
+  #        break
+ #    else:
+ #         print("nie ma takiej komendy")
+
+
+
+#def Ppprostokatu(r):
+#    return a*b
+#while True:
+#     inp = input()
+#     if inp == "a":
+ #         data = float(input())
+ #         print(Ppprostakatu(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+
+
+
+#def Pptrojkata(r):
+#    return 1/2(a*b)
+#while True:
+#     inp = input()
+#     if inp == "a":
+#         data = float(input())
+ #         print(Pptrojkata(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+
+
+
+
+#def Pptrapezu(r):
+#    return 1/2((a+b)h)
+#while True:
+#     inp = input()
+#     if inp == "a":
+ #         data = float(input())
+ #         print(Pptrapezu(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+
+
+
+
+#def Pprombu(r):
+ #   return 1/2(e*f)
+#while True:
+ #    inp = input()
+#     if inp == "a":
+ #         data = float(input())
+  #        print(Pprombu(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+
+
+
+#def Pszescianu(r):
+ #   return a**3
+#while True:
+ #    inp = input()
+ #    if inp == "a":
+ #         data = float(input())
+  #        print(Pszescianu(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+#def obwodkola(r):
+ #   return 2*math.pi*r
+#while True:
+ #    inp = input()
+ #    if inp == "a":
+ #         data = float(input())
+  #        print(obwodkola(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+#def objetosckola(r):
+ #   return 4/3*math.pi*r**3
+#while True:
+ #    inp = input()
+ #    if inp == "a":
+ #         data = float(input())
+  #        print(objetosckola(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+#def Pitagoras(r):
+ #   return a**2*b**2
+#while True:
+ #    inp = input()
+ #    if inp == "a":
+ #         data = float(input())
+  #        print(pitagoras(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+#def Pobjetoscstozka(r):
+ #   return 1/3 *math.pi*r**2*h
+#while True:
+ #    inp = input()
+ #    if inp == "a":
+ #         data = float(input())
+  #        print(objetoscstozka(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+#def objetoscprostopadloscianu(r):
+ #   return a*b*h
+#while True:
+ #    inp = input()
+ #    if inp == "a":
+ #         data = float(input())
+  #        print(objetoscprostopadloscianu(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+#def obwodkola(r):
+ #   return 2*math.pi*r
+#while True:
+ #    inp = input()
+ #    if inp == "a":
+ #         data = float(input())
+  #        print(obwodkola(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+#def Ppszescianu(r):
+ #   return a**2*6
+#while True:
+ #    inp = input()
+ #    if inp == "a":
+ #         data = float(input())
+  #        print(Ppszescianu(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+#def Pkola(r):
+ #   return 2*math.pi*r**2
+#while True:
+ #    inp = input()
+ #    if inp == "a":
+ #         data = float(input())
+  #        print(Pkola(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+
+#def obwodkwadratu(r):
+ #   return a*4
+#while True:
+ #    inp = input()
+ #    if inp == "a":
+ #         data = float(input())
+  #        print(obwodkwadratu(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+#def obwodprostokata(r):
+ #   return a*2+b*2
+#while True:
+ #    inp = input()
+ #    if inp == "a":
+ #         data = float(input())
+  #        print(obwodprostokata(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
+#def obwodtrapezu(r):
+ #   return a+b+c+D
+#while True:
+ #    inp = input()
+ #    if inp == "a":
+ #         data = float(input())
+  #        print(obwodtrapezu(data))
+ #    elif inp == "b":
+ #         break
+ #  else:
+  #       print("nie ma takiej komendy")
